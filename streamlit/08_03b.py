ccbc370755cf173d02f682d4f0b983c5707b4c26
clauds <waguramarie@gmail.com>
2026-04-07 09:25:26 +0200

    streamlit - pie charts

--- streamlit/08_03b.py
+++ streamlit/08_03b.py
@@ -7,8 +7,15 @@ col_names = ['column1', 'column2', 'column3']
 
 data = pd.DataFrame(np.random.randint(30, size=(30,3)), columns = col_names)
 
-'line graph'
+'line graph:'
 st.line_chart(data)
 
 'Bar graph:'
 st.bar_chart(data)
+
+animals = ['cat', 'cow', 'dog']
+heights = [30,150,80]
+
+'Pie Chart:'
+fig, ax = plt.subplots()
+ax.pie(heights, labels=animals)
