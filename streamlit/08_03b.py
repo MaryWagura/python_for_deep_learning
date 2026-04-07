e3497ebf3d7b319710d808dcf14365324edb52f8
clauds <waguramarie@gmail.com>
2026-04-07 09:22:47 +0200

    streamlit - line charts

--- streamlit/08_03b.py
+++ streamlit/08_03b.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
+
+col_names = ['column1', 'column2', 'column3']
+
+data = pd.DataFrame(np.random.randint(30, size=(30,3)), columns = col_names)
+
+'line graph'
+st.line_chart(data)
