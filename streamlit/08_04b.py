7c0f190a0230b3810082d4deb1b15ca20f757981
clauds <waguramarie@gmail.com>
2026-04-08 09:57:51 +0200

    grow line chart

--- streamlit/08_04b.py
+++ streamlit/08_04b.py
@@ -4,4 +4,13 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
+rows = np.random.randn(1,1)
 
+'Growing Line Chart:'
+chart = st.line_chart(rows)
+
+for i in range(1,100):
+    new_rows = rows[0] + np.random.randn(1,1)
+    chart.add_rows(new_rows)
+    rows = new_rows
+    time.sleep(0.05)
