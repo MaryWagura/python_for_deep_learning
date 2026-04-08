0e1103aa9afd491f56e78b82b9675fff421c9b2f
clauds <waguramarie@gmail.com>
2026-04-08 10:14:44 +0200

    create a bar chart in matplot and display in streamlit

--- streamlit/08_05b.py
+++ streamlit/08_05b.py
@@ -1,1 +1,18 @@
+import time
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+#create a bar chart in matplot and display in streamlit
+
+animals = ['cat', 'dog', 'horse', 'girrafe', 'cow']
+heights = [30, 40, 60, 80, 50]
+weights = [10, 30, 50, 80, 100]
+
+fig, ax = plt.subplots()
+
+x= np.arange(len(heights))
+width = 0.40
+
 
